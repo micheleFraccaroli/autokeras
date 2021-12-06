69a307bb409085ddbf62b983d6681c7bc7286ffe
michele fraccaroli <michelefraccaroli@gmail.com>
2021-12-06 17:48:59 +0100

    setting for first test

--- autom.py
+++ autom.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
-
 import autokeras as ak
+from keras_tuner.engine.hyperparameters import Choice
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape)  # (60000, 28, 28)
@@ -13,10 +13,12 @@ max_flops = 140000000
 input_node = ak.ImageInput()
 output_node = ak.Normalization()(input_node)
 output_node = ak.ImageAugmentation(horizontal_flip=False)(output_node)
+output_node = ak.ConvBlock()(output_node)
+output_node = autokeras.ImageBlock()(output_node)
 output_node = ak.ResNetBlock(version="v2")(output_node)
 output_node = ak.ClassificationHead(num_classes=10)(output_node)
 clf = ak.AutoModel(
-    inputs=input_node, outputs=output_node, overwrite=True, max_trials=3, max_model_size={'flops': float(max_flops)}
+    inputs=input_node, outputs=output_node, overwrite=True, max_trials=15, max_model_size={'flops': float(max_flops)}
 )
 
-clf.fit(x_train[:50], y_train[:50], epochs=15)
+clf.fit(x_train, y_train, epochs=50)
